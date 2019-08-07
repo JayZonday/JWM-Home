25dd254ef9c42ecf1d9cc915dec5becd330a0923
JayZonday <jmoloughney25@gmail.com>
2019-08-07 15:40:32 -0400

    logos of favorite sport teams in place -- next need to link them

--- src/components/CentralCarousel/index.js
+++ src/components/CentralCarousel/index.js
@@ -31,13 +31,15 @@ class CentralCarousel extends Component {
             </Carousel.Item>
             <Carousel.Item>
               <div className='team-container'>
-                <div className='team'>Dallas Cowboys</div>
-                <div className='team'>Los Angeles Lakers</div>
-                <div className='team'>New York Yankees</div>
-                <div className='team'>New Jersey Devils</div>
-                <div className='team'>Duke Blue Devils</div>
-                <div className='team'>Notre Dame Fighting Irish</div>
-                <div className='team'>Waldwick Warriors</div>
+                <div className='team-sect-title'> Favorite Teams </div>
+                <div className='team'><img id='sports-logo' src='https://images.vexels.com/media/users/3/141848/isolated/preview/b141601c09d9296a6e6d4083d6960039-dallas-cowboys-american-football-by-vexels.png'/>Dallas Cowboys</div>
+                <div className='team'><img id='sports-logo' src='https://www.stickpng.com/assets/images/58419d0aa6515b1e0ad75a6c.png'/>Los Angeles Lakers</div>
+                <div className='team'><img id='sports-logo' src='https://www.stickpng.com/assets/images/58419c7ba6515b1e0ad75a62.png'/>Brooklyn Nets</div>
+                <div className='team'><img id='sports-logo' src='https://cdn.freebiesupply.com/images/large/2x/new-york-yankees-logo-transparent.png'/>New York Yankees</div>
+                <div className='team'><img id='sports-logo' src='https://www.stickpng.com/assets/images/5a4fbb83da2b4f099b95da16.png'/>New Jersey Devils</div>
+                <div className='team'><img id='sports-logo' src='https://cdn.freebiesupply.com/logos/large/2x/duke-blue-devils-1-logo-png-transparent.png'/>Duke Blue Devils</div>
+                <div className='team'><img id='sports-logo' src='https://cdn.freebiesupply.com/logos/large/2x/notre-dame-fighting-irish-2-logo-png-transparent.png'/>Notre Dame Fighting Irish</div>
+                <div className='team'><img id='sports-logo' src='https://www.waldwickschools.org/cms/lib/NJ01912972/Centricity/Template/GlobalAssets/images///HS/HS%20Warrior%20Logo.png'/>Waldwick Warriors</div>
               </div>
             </Carousel.Item>
           </Carousel>
