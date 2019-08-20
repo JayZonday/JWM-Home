409ce1dae07ef8cbd8fca4fc30189c29d02533fd
JayZonday <jmoloughney25@gmail.com>
2019-08-20 11:11:19 -0400

    new bet of the day -- attempting to go above 500

--- src/components/TopNavbar/index.js
+++ src/components/TopNavbar/index.js
@@ -60,10 +60,10 @@ class TopNavbar extends Component {
               <span id='bet-header-text'>
               Bet of The Day
               <br/>
-              August 18, 2019
+              August 20, 2019
               </span>
               <hr id='bet-text-divider'/>
-              <div id='the-bet'></div>
+              <div id='the-bet'>St. Louis Cardinals (vs Milwaukee Brewers) ML +100</div>
               <br/>
               <div id='bet-record'>2-2 Since August 8, 2019</div>
               <div id='last-bets'>8/8 - San Francisco Giants ML +100 <b>[Won]</b></div>
