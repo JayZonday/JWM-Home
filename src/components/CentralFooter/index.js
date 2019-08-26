71af912a705e6543040d9efd7f100192450d7fc9
JayZonday <jmoloughney25@gmail.com>
2019-08-26 12:54:35 -0400

    footer edited -- img changed to a more fluent icon from prev text icon of spade

--- src/components/CentralFooter/index.js
+++ src/components/CentralFooter/index.js
@@ -5,7 +5,7 @@ class CentralFooter extends Component {
   render(){
     return (
       <div className='footer-section'>
-        <h2 className='title' id='footer'>JayZonday Visions <span id='club'> ♣</span></h2>
+        <h2 className='title' id='footer'>JayZonday Visions <img src='https://www.clipartmax.com/png/full/3-37416_clover-irish-four-leaves-luck-tattoo-white-shamrock-transparent.png' id='club'/> </h2>
         <div className='signature'>Latest Update August 2019 - Designed & Built by <span id='signname'>JayZonday</span></div>
       </div>
     );
