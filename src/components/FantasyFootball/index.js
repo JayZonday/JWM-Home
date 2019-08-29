b0929a1372ebfe50ec417d7b2585a29b63790c22
JayZonday <jmoloughney25@gmail.com>
2019-08-29 19:47:12 -0400

    added further layout of the fantasy football page -- borders will probably be removed and text names of players will eventually be replaced with pictures when a bucket is setup -- temp setup

--- src/components/FantasyFootball/index.js
+++ src/components/FantasyFootball/index.js
@@ -11,7 +11,7 @@ class FantasyFootball extends Component {
         <div className='ff-intro'>
           <div className='ff-intro-box'>
             Welcome to JayZonday Visions 2019 Fantasy Football Hub - Catch all the latest news and advice here!
-            Desperate for a waiver wire addition? - Need to know if a trade is fair? -
+            Desperate for a waiver wire addition?  Need to know if a trade is fair?
             Don't know who to start? <br/> You're in LUCK.
           </div>
 
@@ -26,23 +26,60 @@ class FantasyFootball extends Component {
 
         <div className='ff-rankings-section'>
           <h3 className='ff-section-title' id='greenbg'> Weekly Rankings</h3>
-          <div className='rb-section'></div>
-          <div className='wr-section'></div>
-          <div className='qb-section'></div>
-          <div className='te-section'></div>
+          <div className='rb-section'>
+            <h3 className='rankings-title'>Running Backs</h3>
+          </div>
+          <div className='wr-section'>
+            <h3 className='rankings-title'>Wide Recievers</h3>
+          </div>
+          <div className='qb-section'>
+            <h3 className='rankings-title'>Quarterbacks</h3>
+          </div>
+          <div className='te-section'>
+            <h3 className='rankings-title'>Tight Ends</h3>
+          </div>
         </div>
+
         <div className='ff-waivers-section'>
           <h3 className='ff-section-title' id='whitebg'> Weekly Waiver Wire </h3>
-          <div className='rb-section'></div>
-          <div className='wr-section'></div>
-          <div className='qb-section'></div>
-          <div className='te-section'></div>
+          <div className='rb-section'>
+            <h3 className='waiver-title'>Running Backs</h3>
+          </div>
+          <div className='wr-section'>
+            <h3 className='waiver-title'>Wide Recievers</h3>
+          </div>
+          <div className='qb-section'>
+            <h3 className='waiver-title'>Quarterbacks</h3>
+          </div>
+          <div className='te-section'>
+            <h3 className='waiver-title'>Tight Ends</h3>
+          </div>
         </div>
+
         <div className='ff-trades-section'>
           <h3 className='ff-section-title' id='greenbg'> Trading Block </h3>
+          <div className='buy-section'>
+            <h3 className='trade-title'>Buy Low</h3>
+          </div>
+          <div className='sell-section'>
+            <h3 className='trade-title'>Sell High</h3>
+          </div>
         </div>
+
         <div className='ff-daily-section'>
           <h3 className='ff-section-title' id='whitebg'> Daily Fantasy</h3>
+          <div className='rb-section'>
+            <h3 className='waiver-title'>Running Backs</h3>
+          </div>
+          <div className='wr-section'>
+            <h3 className='waiver-title'>Wide Recievers</h3>
+          </div>
+          <div className='qb-section'>
+            <h3 className='waiver-title'>Quarterbacks</h3>
+          </div>
+          <div className='te-section'>
+            <h3 className='waiver-title'>Tight Ends</h3>
+          </div>
         </div>
 
 
