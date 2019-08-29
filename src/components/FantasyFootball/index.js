ac734fe685318a0c97c274c21efdf568e3b7ca82
JayZonday <jmoloughney25@gmail.com>
2019-08-29 09:49:15 -0400

    fixed title on fantasy football page

--- src/components/FantasyFootball/index.js
+++ src/components/FantasyFootball/index.js
@@ -4,8 +4,13 @@ import './index.css';
 class FantasyFootball extends Component {
   render(){
     return (
-      <div className='fantasy-page-bg'>
+      <div>
+      <div className='fantasy-page-header'>
         <h3 className='ff-title'> Fantasy Football </h3>
+      </div>
+      <div className='ff-intro'>
+
+      </div>
       </div>
     );
   }
