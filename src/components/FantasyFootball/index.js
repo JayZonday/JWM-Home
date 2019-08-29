68292daa1b59d0420e3b5ca18e9040a33e0336bf
JayZonday <jmoloughney25@gmail.com>
2019-08-29 10:59:08 -0400

    laying out fantasy football page -- adding slab sections -- waiver wire -- rankings -- who to start -- trades

--- src/components/FantasyFootball/index.js
+++ src/components/FantasyFootball/index.js
@@ -9,7 +9,11 @@ class FantasyFootball extends Component {
         <h3 className='ff-title'> Fantasy Football </h3>
       </div>
       <div className='ff-intro'>
-
+        <div className='ff-intro-box'>
+          Welcome to JayZonday Visions 2019 Fantasy Football Hub - Catch all the latest news and advice here!
+          Desperate for a waiver wire addition? - Need to know if a trade is fair? -
+          Don't know who to start? - You're in LUCK.
+        </div>
       </div>
       </div>
     );
