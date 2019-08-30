46d060a62c64914c20cf01b8909abc01682f1cfe
JayZonday <jmoloughney25@gmail.com>
2019-08-30 03:26:21 -0400

    new fantasy football page layout

--- src/components/FantasyFootball/index.js
+++ src/components/FantasyFootball/index.js
@@ -11,8 +11,8 @@ class FantasyFootball extends Component {
         <div className='ff-intro'>
           <div className='ff-intro-box'>
             Welcome to JayZonday Visions 2019 Fantasy Football Hub - Catch all the latest news and advice here!
-            Desperate for a waiver wire addition?  Need to know if a trade is fair?
-            Don't know who to start? <br/> You're in LUCK.
+            Desperate for a waiver wire addition?  Need a trade option to target?
+            Don't know who to start this week? <br/> You're in <span className='luck'>LUCK</span>
           </div>
 
           <div className='ff-nav'>
@@ -28,15 +28,75 @@ class FantasyFootball extends Component {
           <h3 className='ff-section-title' id='greenbg'> Weekly Rankings</h3>
           <div className='rb-section'>
             <h3 className='rankings-title'>Running Backs</h3>
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+              <div>5</div>
+              <div>6</div>
+              <div>7</div>
+              <div>8</div>
+              <div>9</div>
+              <div>10</div>
+              <div>11</div>
+              <div>12</div>
+              <div>13</div>
+              <div>14</div>
+              <div>15</div>
           </div>
           <div className='wr-section'>
             <h3 className='rankings-title'>Wide Recievers</h3>
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+              <div>5</div>
+              <div>6</div>
+              <div>7</div>
+              <div>8</div>
+              <div>9</div>
+              <div>10</div>
+              <div>11</div>
+              <div>12</div>
+              <div>13</div>
+              <div>14</div>
+              <div>15</div>
           </div>
           <div className='qb-section'>
             <h3 className='rankings-title'>Quarterbacks</h3>
+            <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+              <div>5</div>
+              <div>6</div>
+              <div>7</div>
+              <div>8</div>
+              <div>9</div>
+              <div>10</div>
+              <div>11</div>
+              <div>12</div>
+              <div>13</div>
+              <div>14</div>
+              <div>15</div>
           </div>
           <div className='te-section'>
             <h3 className='rankings-title'>Tight Ends</h3>
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+              <div>5</div>
+              <div>6</div>
+              <div>7</div>
+              <div>8</div>
+              <div>9</div>
+              <div>10</div>
+              <div>11</div>
+              <div>12</div>
+              <div>13</div>
+              <div>14</div>
+              <div>15</div>
           </div>
         </div>
 
@@ -44,15 +104,35 @@ class FantasyFootball extends Component {
           <h3 className='ff-section-title' id='whitebg'> Weekly Waiver Wire </h3>
           <div className='rb-section'>
             <h3 className='waiver-title'>Running Backs</h3>
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+              <div>5</div>
           </div>
           <div className='wr-section'>
             <h3 className='waiver-title'>Wide Recievers</h3>
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+              <div>5</div>
           </div>
           <div className='qb-section'>
             <h3 className='waiver-title'>Quarterbacks</h3>
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+              <div>5</div>
           </div>
           <div className='te-section'>
             <h3 className='waiver-title'>Tight Ends</h3>
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+              <div>5</div>
           </div>
         </div>
 
@@ -70,15 +150,45 @@ class FantasyFootball extends Component {
           <h3 className='ff-section-title' id='whitebg'> Daily Fantasy</h3>
           <div className='rb-section'>
             <h3 className='waiver-title'>Running Backs</h3>
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+              <div>5</div>
+              <div>6</div>
+              <div>7</div>
+              <div>8</div>
+              <div>9</div>
+              <div>10</div>
           </div>
           <div className='wr-section'>
             <h3 className='waiver-title'>Wide Recievers</h3>
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+              <div>5</div>
+              <div>6</div>
+              <div>7</div>
+              <div>8</div>
+              <div>9</div>
+              <div>10</div>
           </div>
           <div className='qb-section'>
             <h3 className='waiver-title'>Quarterbacks</h3>
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+              <div>5</div>
           </div>
           <div className='te-section'>
             <h3 className='waiver-title'>Tight Ends</h3>
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+              <div>5</div>
           </div>
         </div>
 
