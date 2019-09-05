b3cb5443220c65de2408262653b49e6a8ea687be
JayZonday <jmoloughney25@gmail.com>
2019-09-05 11:30:57 -0400

    smaller footer date change

--- src/components/CentralFooter/index.js
+++ src/components/CentralFooter/index.js
@@ -6,7 +6,7 @@ class CentralFooter extends Component {
     return (
       <div className='footer-section'>
         <h2 className='title' id='footer'>JayZonday Visions <img src='https://www.clipartmax.com/png/full/3-37416_clover-irish-four-leaves-luck-tattoo-white-shamrock-transparent.png' id='club'/> </h2>
-        <div className='signature'>Latest Update August 2019 - Designed & Created by <span id='signname'>JayZonday</span></div>
+        <div className='signature'>Latest Update September 2019 - Designed & Created by <span id='signname'>JayZonday</span></div>
       </div>
     );
   }
