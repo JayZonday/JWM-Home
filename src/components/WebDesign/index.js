30ba64c25240f13e6bc9578b1c55d0a3b848e370
JayZonday <jmoloughney25@gmail.com>
2019-10-07 12:29:23 -0400

    building out first design template

--- src/components/WebDesign/index.js
+++ src/components/WebDesign/index.js
@@ -16,14 +16,7 @@ class WebDesign extends Component {
 
 
 
-        <div className='code-design'>
-          <h3 className='code-test-title'>Technical Challenge</h3>
-          <div className='code-test-div'>
-            <button className='code-test-btn'> Test Button</button>
-            <div className='test-display'>
-            </div>
-          </div>
-        </div>
+
 
 
 
@@ -31,7 +24,17 @@ class WebDesign extends Component {
 
 
         <div className='design-2'>
-          <h3 className='design-2-title'>JJs Pizzeria</h3>
+          <div className='design-2-header'>
+            <h3 className='design-2-title'>JJs Pizzeria < hr className='design-2-titleline' /></h3>
+            <div className='design-2-deal'><div className='the-deal'> 1 Large Pie and 2L Soda <span className='price'>$19.99</span></div></div>
+          </div>
+          <div className='design-2-navbar'>
+            <div className='design-2-navbtn'>About Us</div>
+            <div className='design-2-navbtn'>Locations</div>
+            <div className='design-2-navbtn'>Menu</div>
+            <div className='design-2-navbtn'>Catering</div>
+            <div className='design-2-navbtn'>Employment</div>
+          </div>
         </div>
         <div className='design-3'>
           <h3 className='design-3-title'>JJs Pizzeria</h3>
@@ -41,6 +44,14 @@ class WebDesign extends Component {
         </div>
         <div className='design-5'>
           <h3 className='design-5-title'>JJs Pizzeria</h3>
+        </div>
+        <div className='code-design'>
+          <h3 className='code-test-title'>Technical Challenge</h3>
+          <div className='code-test-div'>
+            <button className='code-test-btn'> Test Button</button>
+            <div className='test-display'>
+            </div>
+          </div>
         </div>
 
       </div>
