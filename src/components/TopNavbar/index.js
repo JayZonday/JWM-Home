d0e99561b245be48e3d9dd6303c874492f71a378
JayZonday <jmoloughney25@gmail.com>
2019-10-23 09:15:49 -0400

    bet change -- lakers lost -- overall record now moves to 11-6 since august 8th

--- src/components/TopNavbar/index.js
+++ src/components/TopNavbar/index.js
@@ -48,12 +48,12 @@ class TopNavbar extends Component {
               <span id='bet-header-text'>
               Bet of The Day
               <br/>
-              October 22, 2019
+              October 23, 2019
               </span>
               <hr id='bet-text-divider'/>
-              <div id='the-bet'>Los Angeles Lakers -7 -110 (@ Los Angeles Clippers)</div>
+              <div id='the-bet'></div>
               <br/>
-              <div id='bet-record'>11-5 Since August 8, 2019</div>
+              <div id='bet-record'>11-6 Since August 8, 2019</div>
               <div id='last-bets'>8/8 - San Francisco Giants ML +100 <b>[Won]</b></div>
               <div id='last-bets'>8/9 - New York Mets ML +115 <b>[Won]</b></div>
               <div id='last-bets'>8/11 - Philadelphia Phillies ML -115 <b>[Lost]</b></div>
@@ -70,6 +70,7 @@ class TopNavbar extends Component {
               <div id='last-bets'>10/15 - New York Yankees ML +140 <b>[Lost]</b></div>
               <div id='last-bets'>10/16 - Pittsburgh Penguins ML -115 <b>[Won]</b></div>
               <div id='last-bets'>10/21 - New England Patriots -5.5 1-Half -115 <b>[Won]</b></div>
+              <div id='last-bets'>10/22 - Los Angeles Lakers -3 -110 <b>[Lost]</b></div>
 
             </div>
           </div>
