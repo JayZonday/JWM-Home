b109211bfab3f083c0e78e7b2ecff3a396f87da6
JayZonday <jmoloughney25@gmail.com>
2020-01-14 13:41:35 -0500

    removed dallas for now also trying to fix mobile view

--- src/components/CentralCarousel/index.js
+++ src/components/CentralCarousel/index.js
@@ -105,19 +105,6 @@ class CentralCarousel extends Component {
                 </a>
               </div>
               </Carousel.Item>
-
-            <Carousel.Item>
-            <div className='dallas-appreciation-title'>Dallas Appreciation Gallery</div>
-            <div className='large-container'>
-
-              <div className='c-item'><img src='https://i.ibb.co/TKSC609/4-E695593-8-B8-A-4636-B22-D-84-F9-B912-C740.jpg' id='dallas-img'/></div>
-              <div className='c-item'><img src='https://i.ibb.co/pRtDzmR/DF8-C2189-9596-432-D-BEDE-D0-BD63-EE5795.jpg' id='dallas-img'/></div>
-              <div className='c-item'><img src='https://i.ibb.co/8MG09Yy/Dallas.jpg' id='dallas-img'/></div>
-              <div className='c-item'><img src='https://i.ibb.co/rtSKS6x/BF8-A6828-2258-4-F85-91-E1-76-BABA7-BAC4-C.jpg' id='dallas-img'/></div>
-              <div className='c-item'><img src='https://i.ibb.co/RGKQfKf/2-C609489-502-C-4-C4-F-AC72-C752-A9-DEFBEB.jpg' id='dallas-img'/></div>
-              <div className='c-item'><img src='https://i.ibb.co/YcrynV9/21-D8-FCC8-0333-437-B-99-A5-69826-BC06-F7-C.jpg' id='dallas-img'/></div>
-            </div>
-            </Carousel.Item>
           </Carousel>
         </div>
 
